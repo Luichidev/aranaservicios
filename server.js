b394d75138d590590986c4e39ecb5a0a7987e07d
Luis Alberto <disconluis@gmail.com>
2019-01-14 14:57:08 +0100

    fixed server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
@@ -13,4 +15,9 @@ app.get('*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/ASII/index.html'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, function() {
+  console.log("Server started.......");
+});

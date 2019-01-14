5c8dba74b1f1244e3801b4fd687aa80550dde365
Luis Alberto <disconluis@gmail.com>
2019-01-14 21:18:20 +0100

    fixed error code:H10

--- server.js
+++ server.js
@@ -3,21 +3,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/ASII'));
+app.use(express.static(__dirname + '/dist/asii'));
 
-app.get('*', function(req,res) {
+app.get('/*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/ASII/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/asii/index.html'));
 });
 // Start the app by listening on the default Heroku port
-const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
 
-app.listen(port, host, function() {
-  console.log("Server started.......");
+app.listen(process.env.PORT || 8080, ()=> {
+  console.log('Server started...')
 });
